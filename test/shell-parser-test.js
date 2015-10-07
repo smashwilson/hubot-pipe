5598955ca2df174b188db40ed681a982d3bcfc0f
Ash Wilson <smashwilson@gmail.com>
2015-10-07 11:37:00 -0400

    More tests for different subshell structures.

--- test/shell-parser-test.js
+++ test/shell-parser-test.js
@@ -7,24 +7,55 @@ describe("shell-parser", function () {
 
   it("passes commands with no special shell concepts as-is", function () {
     var expr = shellParser.parse("@hubot pug me");
-    
+
     expect(expr.dump()).to.equal("(PipeSequence (Command (CommandPart [@hubot pug me])))");
   });
 
-  it("identifies subshell invocations", function () {
-    var expr = shellParser.parse("@hubot echo $(@hubot pug me) is a pug image");
+  describe("subshells", function () {
 
-    var expected = "(PipeSequence (Command " +
-      "(CommandPart [@hubot echo ]) " +
-      "(PipeSequence (Command (CommandPart [@hubot pug me]))) " +
-      "(CommandPart [ is a pug image])))";
-    expect(expr.dump()).to.equal(expected);
+    it("identifies subshell invocations", function () {
+      var expr = shellParser.parse("@hubot echo $(@hubot pug me) is a pug image");
+
+      var expected = "(PipeSequence (Command " +
+        "(CommandPart [@hubot echo ]) " +
+        "(PipeSequence (Command (CommandPart [@hubot pug me]))) " +
+        "(CommandPart [ is a pug image])))";
+      expect(expr.dump()).to.equal(expected);
+    });
+
+    it("identifies multiple subshells", function () {
+      var expr = shellParser.parse("hubot echo $(hubot echo one) and $(hubot echo two)");
+
+      var expected = "(PipeSequence (Command " +
+        "(CommandPart [hubot echo ]) " +
+        "(PipeSequence (Command (CommandPart [hubot echo one]))) " +
+        "(CommandPart [ and ]) " +
+        "(PipeSequence (Command (CommandPart [hubot echo two])))" +
+        "))";
+      expect(expr.dump()).to.equal(expected);
+    });
+
+    it("parses nested subshells", function () {
+      var expr = shellParser.parse("hubot echo 1 $(hubot echo 2 $(hubot echo 3 foo))");
+
+      var expected = "(PipeSequence (Command " +
+        "(CommandPart [hubot echo 1 ]) " +
+        "(PipeSequence (Command " +
+          "(CommandPart [hubot echo 2 ]) " +
+          "(PipeSequence (Command " +
+            "(CommandPart [hubot echo 3 foo])" +
+            "))" +
+          "))" +
+        "))";
+      expect(expr.dump()).to.equal(expected);
+    });
+
   });
 
   it("identifies a pipe sequence");
 
   it("parses a pipe within a subshell");
-  
+
   it("parses a subshell within a pipe");
 
 });
