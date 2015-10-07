e6fe59561dcecb25baff16ab148e9deb9875cab7
Ash Wilson <smashwilson@gmail.com>
2015-10-07 11:45:51 -0400

    Refactor common bits out of the tests.

--- test/shell-parser-test.js
+++ test/shell-parser-test.js
@@ -3,28 +3,41 @@ var expect = require('chai').expect;
 var shellParser = require('../src/shell-parser');
 var ast = require('../src/ast');
 
-describe("shell-parser", function () {
+describe("shellParser", function () {
 
-  it("passes commands with no special shell concepts as-is", function () {
-    var expr = shellParser.parse("@hubot pug me");
+  var parsesAs = function (original, expected) {
+    var expr = shellParser.parse(original);
+    expect(expr.dump()).to.equal(expected);
+  };
 
-    expect(expr.dump()).to.equal("(PipeSequence (Command (CommandPart [@hubot pug me])))");
+  describe("plain commands", function () {
+
+    it("passes commands with no special shell concepts as-is", function () {
+      parsesAs("@hubot pug me", "(PipeSequence (Command (CommandPart [@hubot pug me])))");
+    });
+
+    it("accepts ( in a normal command");
+
+    it("accepts $ in a normal command");
+
+    it("accepts ) in a normal command");
+
   });
 
   describe("subshells", function () {
 
     it("identifies subshell invocations", function () {
-      var expr = shellParser.parse("@hubot echo $(@hubot pug me) is a pug image");
+      var input = "@hubot echo $(@hubot pug me) is a pug image";
 
       var expected = "(PipeSequence (Command " +
         "(CommandPart [@hubot echo ]) " +
         "(PipeSequence (Command (CommandPart [@hubot pug me]))) " +
         "(CommandPart [ is a pug image])))";
-      expect(expr.dump()).to.equal(expected);
+      parsesAs(input, expected);
     });
 
     it("identifies multiple subshells", function () {
-      var expr = shellParser.parse("hubot echo $(hubot echo one) and $(hubot echo two)");
+      var input = "hubot echo $(hubot echo one) and $(hubot echo two)";
 
       var expected = "(PipeSequence (Command " +
         "(CommandPart [hubot echo ]) " +
@@ -32,11 +45,12 @@ describe("shell-parser", function () {
         "(CommandPart [ and ]) " +
         "(PipeSequence (Command (CommandPart [hubot echo two])))" +
         "))";
-      expect(expr.dump()).to.equal(expected);
+
+      parsesAs(input, expected);
     });
 
     it("parses nested subshells", function () {
-      var expr = shellParser.parse("hubot echo 1 $(hubot echo 2 $(hubot echo 3 foo))");
+      var input = "hubot echo 1 $(hubot echo 2 $(hubot echo 3 foo))";
 
       var expected = "(PipeSequence (Command " +
         "(CommandPart [hubot echo 1 ]) " +
@@ -47,7 +61,7 @@ describe("shell-parser", function () {
             "))" +
           "))" +
         "))";
-      expect(expr.dump()).to.equal(expected);
+      parsesAs(input, expected);
     });
 
   });
