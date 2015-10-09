b94e0aed27a010de1b0d3616888b568bd0f7b7d8
Ash Wilson <smashwilson@gmail.com>
2015-10-09 12:00:04 -0400

    Utility to shim TextMessages

---
+++ src/messenger.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var Messenger = module.exports = function (message) {
+  this.message = message;
+};
+
+Messenger.isTextMessage = function (message) {
+  return message.constructor.name === 'TextMessage';
+};
+
+Messenger.prototype.create = function (text) {
+  var m = Object.create(this.message);
+  m.text = text;
+  m.done = false;
+  return m;
+};
+
+Messenger.prototype.makeEnvelope = function () {
+  return { room: this.message.room, user: this.message.user, message: this.message };
+}
