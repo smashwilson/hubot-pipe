9fccfb7ba736f2bdddcddfb79b0a55f899825d48
Ash Wilson <smashwilson@gmail.com>
2015-10-11 20:09:40 -0400

    Capture Part output differently than Pipe output.

--- src/capture.js
+++ src/capture.js
@@ -1,5 +1,61 @@
 "use strict";
 
+function PipeOutput (msg) {
+  this.queue = [msg];
+};
+
+PipeOutput.prototype.isEmpty = function () {
+  return this.queue.length === 0;
+};
+
+PipeOutput.prototype.push = function (envelope, msg) {
+  if (envelope.static) {
+    throw new Error("Attempt to push static output onto a PipeOutput");
+  }
+
+  this.queue.push(msg);
+
+  return this;
+};
+
+PipeOutput.prototype.next = function () {
+  return this.queue.shift();
+};
+
+function PartOutput (msg) {
+  this.text = msg;
+};
+
+PartOutput.prototype.isEmpty = function () {
+  return false;
+};
+
+PartOutput.prototype.push = function () {
+  throw new Error("Attempt to push additional output on top of a Part: " + this.text);
+};
+
+PartOutput.prototype.next = function () {
+  return this.text;
+};
+
+function EmptyOutput () {};
+
+EmptyOutput.prototype.isEmpty = function () {
+  return true;
+};
+
+EmptyOutput.prototype.push = function (envelope, msg) {
+  if (envelope.static) {
+    return new PartOutput(msg);
+  } else {
+    return new PipeOutput(msg);
+  }
+};
+
+EmptyOutput.prototype.next = function () {
+  throw new Error("Attempt to read output from an EmptyOutput");
+}
+
 var Capture = module.exports = function (robot) {
   this.robot = robot;
   this.captured = [];
@@ -15,12 +71,12 @@ Capture.prototype.patchedRobot = function (id) {
   var self = this;
   var patchedAdapter = Object.create(this.robot.adapter);
 
-  this.captured[id] = [];
+  this.captured[id] = new EmptyOutput();
 
-  patchedAdapter.send = function () {
+  patchedAdapter.send = function (envelope) {
     var strings = Array.prototype.slice.call(arguments, 1);
     strings.forEach(function (arg) {
-      self._capture(id, arg);
+      self._capture(id, envelope, arg);
     });
   };
 
@@ -38,8 +94,8 @@ Capture.prototype.patchedRobot = function (id) {
   return patchedRobot;
 };
 
-Capture.prototype._capture = function (id, msg) {
-  this.captured[id].push(msg);
+Capture.prototype._capture = function (id, envelope, msg) {
+  this.captured[id] = this.captured[id].push(envelope, msg);
   this._checkCompletion();
 };
 
@@ -49,7 +105,7 @@ Capture.prototype._isComplete = function () {
   }
 
   return ! this.captured.some(function (each) {
-    return each.length === 0;
+    return each.isEmpty();
   });
 }
 
@@ -59,7 +115,7 @@ Capture.prototype._checkCompletion = function () {
 
     // TODO: this should actually handle permutations, somehow
     this.captured.forEach(function (each, i) {
-      result[i] = each.shift();
+      result[i] = each.next();
     }.bind(this));
 
     this.completeCallback(null, result);
